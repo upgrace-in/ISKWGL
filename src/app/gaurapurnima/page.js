d527e6fc5392742cf7f3727fada0cda1881f8c1e
Suraj Baradhi <suraj@Surajs-MacBook-Air.local>
2025-03-14 12:06:13 +0530

    Suraj:Changing default donation option for Gaura Purnima

--- src/app/gaurapurnima/page.js
+++ src/app/gaurapurnima/page.js
@@ -14,8 +14,8 @@ export default function Page({ params }) {
 
         payingOptions={
             [
-                { title: "Prasadam Seva (15 devotees)", amount: 1516, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
                 { title: "Prasadam Seva (30 devotees)", amount: 3116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
+                { title: "Prasadam Seva (15 devotees)", amount: 1516, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
                 { title: "Prasadam Seva (60 devotees)", amount: 6116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
                 { title: "Prasadam Seva (100 devotees)", amount: 10116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
                 { title: "Puspa Alankar Seva", amount: 2116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' }
