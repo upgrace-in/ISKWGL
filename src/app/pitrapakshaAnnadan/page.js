5dc5953baa036cc878466c7d469d2a6bbd2c6e5f
saurabh sahu <saurabh.ss.sahu@oracle.com>
2025-09-13 20:42:37 +0530

    Add pitrapaksha annadan

---
+++ src/app/pitrapakshaAnnadan/page.js
@@ -0,0 +1,6 @@
+'use client'
+
+import PitrapakshaAnndan from "@/Components/PitrapakshaAnnadan"
+export default function Page({ params }) {
+    return <PitrapakshaAnndan {...params} defaultReferral={"facebook"} />
+}
