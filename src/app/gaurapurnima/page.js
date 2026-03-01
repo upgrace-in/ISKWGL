77b5d10a5d5e7ed5b9ae4e8133ac0d05f6edc6fd
DIwakar Bansal <diwakarbansal470@gmail.com>
2026-03-01 09:56:54 +0530

    updating gaur purnima donation options

--- src/app/gaurapurnima/page.js
+++ src/app/gaurapurnima/page.js
@@ -14,11 +14,16 @@ export default function Page({ params }) {
 
         payingOptions={
             [
-                { title: "Prasadam Seva (30 devotees)", amount: 3116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
-                { title: "Prasadam Seva (15 devotees)", amount: 1516, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
-                { title: "Prasadam Seva (60 devotees)", amount: 6116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
-                { title: "Prasadam Seva (100 devotees)", amount: 10116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' },
-                { title: "Puspa Alankar Seva", amount: 2116, imagePath: '/donateForIMGs/gaura-purnima-donation-image.jpeg' }
+                { title: "Full Day Arati Seva", amount: 2516, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Full Day Naivedyam Seva", amount: 3516, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Maha Abhishekam Seva", amount: 7516, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Flowers - Garlands & Decoration", amount: 10116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Prasadam Seva (10 devotees)", amount: 1116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Prasadam Seva (50 devotees)", amount: 5116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Prasadam Seva (100 devotees)", amount: 10116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Prasadam Seva (250 devotees)", amount: 25116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Prasadam Seva (500 devotees)", amount: 50116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' },
+                { title: "Sampoorna Seva", amount: 100116, imagePath: '/donateForIMGs/gaura-purnima-donation-image-1.jpeg' }
             ]
         }
 
