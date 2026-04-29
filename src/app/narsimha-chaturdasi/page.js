b1a01008933fdabc658596aa62156d53ddfc0816
Saurabh Sahu <sauarbh.sahu622@gmail.com>
2026-04-29 09:57:32 +0530

    update the narsimha poster and donation page

--- src/app/narsimha-chaturdasi/page.js
+++ src/app/narsimha-chaturdasi/page.js
@@ -13,12 +13,15 @@ export default function Page({ params }) {
 
         payingOptions={
             [
-                { title: "Prasadam Seva (60 devotees)", amount: 6116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
-                { title: "Prasadam Seva (30 devotees)", amount: 3116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
-                { title: "Prasadam Seva (15 devotees)", amount: 1516, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
+                { title: "Prasadam Seva (10 devotees)", amount: 1116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
+                { title: "Prasadam Seva (50 devotees)", amount: 5116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
                 { title: "Prasadam Seva (100 devotees)", amount: 10116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
-                { title: "Puspa Alankar Seva", amount: 2116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' }
+                { title: "Prasadam Seva (150 devotees)", amount: 15116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
+                { title: "Full day Arti and Naivedya Seva", amount: 2116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
+                { title: "Maha Abhishekam Seva", amount: 3516, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' },
+                { title: "Flower, Garlands and Decoration", amount: 7116, imagePath: '/donateForIMGs/narsimha-chaturdasi-poster.jpeg' }
             ]
+
         }
 
         fields={
