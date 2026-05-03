6acedd8a79f78c9ee3af03a50f1738d838f70576
DIwakar Bansal <diwakarbansal470@gmail.com>
2026-05-03 13:05:52 +0530

    AnnaDaan

---
+++ src/app/AnnaDaan/page.js
@@ -0,0 +1,12 @@
+import FoodForLife from './foodforlife';
+
+export const metadata = {
+  title: 'Food For Life | ISKCON Warangal',
+  description: 'Donate to the Food For Life program.',
+};
+
+export default function Page() {
+  return (
+    <FoodForLife />
+  );
+}
