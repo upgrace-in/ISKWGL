1ebf1e7bd233a54bd505bc08a6d68de2dd441a9a
DIwakar Bansal <diwakarbansal470@gmail.com>
2026-06-15 11:14:23 +0530

    added tracking on website for users

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "@/globals.css";
 import { DonationProvider } from '@/Helpers/DonationContext';
+import Script from 'next/script';
 export const metadata = {
   title: "ISKCON WARANGAL",
   description: "SHRI SHRI RADHA NILMADHAV MANDIR",
@@ -21,6 +22,18 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RRN3XB5GXQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RRN3XB5GXQ');
+        `}
+      </Script>
       <body>
         <DonationProvider>{children}</DonationProvider>
       </body>
