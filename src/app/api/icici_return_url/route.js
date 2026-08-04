a425531cbacd78fba06abf2e1d3ac2221b359684
DIwakar Bansal <diwakarbansal470@gmail.com>
2026-08-04 21:15:09 +0530

    Easy Page

--- src/app/api/icici_return_url/route.js
+++ src/app/api/icici_return_url/route.js
@@ -29,6 +29,7 @@ export async function POST(request) {
         const donation = await Donation.findOne({ orderId: responseData.addlParam1 });
         if (!donation) throw "No Records Exists";
         donation.status = responseData.respDescription === "Transaction successful" ? "SUCCESS" : "FAILED";
+        donation.markModified('status');
         await donation.save();
 
         let new_donation = new TotalDonations({
