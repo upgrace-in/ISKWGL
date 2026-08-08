d389bc8c02af9f3abe2129722cce5e7b32a72d43
DIwakar Bansal <diwakarbansal470@gmail.com>
2026-08-08 09:51:13 +0530

    EKadasi

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -176,16 +176,16 @@ export default function Header() {
 
                 {/* Action Buttons */}
                 <div style={{ display: 'flex', gap: '15px', alignItems: 'center', flex: '2', justifyContent: 'flex-end' }}>
-                    {/* <Link 
-                        href="/AnnaDaan" 
+                    <Link 
+                        href="/Ekadasi" 
                         className={"nav-rath-yatra-highlight"}
                     >
-                        Guru Purnima(29 July)
-                    </Link> */}
+                        Kamika Ekadasi (9 Aug)
+                    </Link>
 
-                    <button style={buttonStyle} onClick={() => window.location.href='/#donate'}>
+                    {/* <button style={buttonStyle} onClick={() => window.location.href='/#donate'}>
                         <FaHeart /> Donate Now
-                    </button>
+                    </button> */}
                     <button style={buttonStyle} onClick={() => window.location.href='/templerenovation'}>
                         <FaHandsHelping size={18} /> Help Temple Renovate
                     </button>
@@ -205,14 +205,14 @@ export default function Header() {
 
                 {/* 2. CENTER: Rath Yatra Button */}
                 {/* flex: none ensures it stays perfectly centered without being stretched */}
-                {/* <div style={{ flex: 'none', display: 'flex', justifyContent: 'center' }}>
+                <div style={{ flex: 'none', display: 'flex', justifyContent: 'center' }}>
                     <Link 
-                        href="/AnnaDaan"
+                        href="/Ekadasi"
                         className={"nav-rath-yatra-highlight mobile-rath-btn"}
                     >
-                        Guru Purnima(29 July)
+                        Kamika Ekadasi (9 Aug)
                     </Link>
-                </div> */}
+                </div>
 
                 {/* 3. RIGHT: 3 Dots / Hamburger */}
                 {/* flex: 1 matches the logo side so the center item stays dead center */}
